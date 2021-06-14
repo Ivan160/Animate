94de9da76fb86f5d9b02e5e3cbf08d6e36fa8abc
Ivan160 <56685399+Ivan160@users.noreply.github.com>
2021-06-14 23:06:06 +1000

    Update scroll.js

--- src/modules/scroll.js
+++ src/modules/scroll.js
@@ -1,30 +1,24 @@
-import animate from './animate';
-var scroll = function () {
-   function scrolling() {
-      var wind = window.pageYOffset;
-      var hash = this.getAttribute('href').slice(1);
-      var hashTop;
-      if (hash) hashTop = document.getElementById(hash).getBoundingClientRect().top;
-      else if (!hash) hashTop = -wind;
-      animate({
-         duration: 500,
-         timing: function (timeFraction) {
-            return timeFraction;
-         },
-         draw: function (progress) {
-            if (hashTop > wind) {
-               window.scrollTo(0, hashTop * progress + wind);
-            } else if (hashTop < wind) {
-               window.scrollTo(0, hashTop * progress + wind);
-            } else if (hashTop == wind) {
-               return;
-            }
-         }
-      });
-   }
-   var hrefAll = document.querySelectorAll('[href^="#"]');
-   for (var i = 0; i < hrefAll.length; i++) {
-      hrefAll[i].addEventListener('click', scrolling);
-   }
+import { animate } from './animate';
+
+
+export const scrolling = (hash) => {
+  const wind = window.pageYOffset;
+  const hashTop = hash ? document.getElementById(hash).getBoundingClientRect().top : -wind;
+
+  animate({
+    duration: 500,
+    timing: (timeFraction) => timeFraction,
+    draw: (progress) => {
+      window.scrollTo(0, hashTop * progress + wind);
+    }
+  });
 };
-export default scroll;
+
+export const scroll = () => {
+  const handleLinkClick = (event) => {
+    scrolling(event.currentTarget.getAttribute('href').slice(1));
+  };
+
+  const hrefAll = document.querySelectorAll('[href^="#"]');
+  hrefAll.forEach((elem) => elem.addEventListener('click', handleLinkClick));
+};
